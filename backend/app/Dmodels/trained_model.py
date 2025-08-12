20740fd439d5ff75137c041d2ba3637864681f37
unknown <elgatobanban@gmail.com>
2025-08-12 16:25:55 -0500

    refactor: data base changed to MySQL

--- backend/app/Dmodels/trained_model.py
+++ backend/app/Dmodels/trained_model.py
@@ -4,7 +4,7 @@ from app.database import Base
 class TrainedModel(Base):
     __tablename__ ="models"
     id = Column(Integer, primary_key=True, index=True)
-    name= Column(String, nullable=False)
+    name= Column(String(255), nullable=False)
     config= Column(JSON, nullable=False)
     metrics= Column(JSON, nullable=False)
     created_at =Column(DateTime, server_default=func.now())
--- backend/app/models.py
+++ backend/app/models.py
@@ -5,15 +5,15 @@ from .database import Base
 class Dataset(Base):
     __tablename__ = "datasets"
     id= Column(Integer, primary_key=True, index=True)
-    name = Column(String, index=True)
-    description = Column(String, index=True)
+    name = Column(String(255), index=True)
+    description = Column(String(500), index=True)
     columns= Column(JSON)
     uploaded_at= Column(DateTime, default=lambda: datetime.now(timezone.utc))
     
 class MLModel(Base):
     __tablename__= "MLModels"
     id = Column(Integer, primary_key=True)
-    name = Column(String)
+    name = Column(String(255))
     config = Column(JSON)
     metrics = Column(JSON)
     created_at = Column(DateTime, default=lambda: datetime.now(timezone.utc))
